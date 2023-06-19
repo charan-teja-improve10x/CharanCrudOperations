475ae8d72994f3c2edf49a79764f465dec6f4d4f
CHARAN TEJA <charan.teja@improve10x.com>
2023-06-19 23:29:05 +0530

    created new activity and used dataBinding to bind the data with ui components

--- app/src/main/java/com/example/charancrudopetrations/userdetails/UserDetailsActivity.java
+++ app/src/main/java/com/example/charancrudopetrations/userdetails/UserDetailsActivity.java
@@ -6,20 +6,35 @@ import android.os.Bundle;
 
 import com.example.charancrudopetrations.R;
 import com.example.charancrudopetrations.databinding.ActivityUserDetailsBinding;
+import com.example.charancrudopetrations.model.User;
 
 public class UserDetailsActivity extends AppCompatActivity {
 
     private ActivityUserDetailsBinding binding;
+    private User user;
+    private Boolean showSave = false;
+    private Boolean showDelete = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityUserDetailsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        checkReceivedData();
+        showData();
+    }
+
+    private void checkReceivedData() {
         if (getIntent().hasExtra("randomUser")) {
-            getIntent().getSerializableExtra("randomUser");
+            user = (User) getIntent().getSerializableExtra("randomUser");
+            binding.setShowSave(true);
         } else if (getIntent().hasExtra("user")) {
-            getIntent().getSerializableExtra("user");
+            user = (User) getIntent().getSerializableExtra("user");
+            binding.setShowDelete(true);
         }
+    }
+
+    private void showData() {
+        binding.setUser(user);
     }
 }
